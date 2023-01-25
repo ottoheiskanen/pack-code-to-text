ac26a9ccfdf501b9cb91c58ebe7d702879d60b7c
kahvimorso <ottoheiskanen@gmail.com>
2023-01-25 20:24:46 +0200

    commit...

--- src/HandleFiles.java
+++ src/HandleFiles.java
@@ -1,5 +1,4 @@
-import java.io.File;
-import java.io.FilenameFilter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -8,7 +7,7 @@ public class HandleFiles {
     private String newFileLocation;
     private ArrayList<String> filePaths;
 
-    public HandleFiles(String newFileName, String newFileLocation, ArrayList<String> filePaths) {
+    public HandleFiles(String newFileName, String newFileLocation, ArrayList<String> filePaths) throws IOException {
         this.newFileName = newFileName;
         this.newFileLocation = newFileLocation;
         this.filePaths = filePaths;
@@ -16,7 +15,7 @@ public class HandleFiles {
         this.readFilesFromPath( listFilesFromEach() );
     }
 
-    // Get all .java files inside given file paths and save them to arraylist arrays
+    // Get all files inside given file paths and save them to arraylist arrays
     public ArrayList<String[]> listFilesFromEach() {
         ArrayList<String[]> filesFromEach = new ArrayList<>();
         for (int i = 0; i < this.filePaths.size(); i++) {
@@ -33,33 +32,65 @@ public class HandleFiles {
         return filesFromEach;
     }
 
-    public void readFilesFromPath(ArrayList<String[]> files) {
+    //read given .java file
+    public String readJavaFile(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader( path ));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+        String ls = System.getProperty("line.separator");
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(ls);
+        }
+
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+        reader.close();
+
+        String javaCode = stringBuilder.toString();
+
+        return javaCode;
+    }
+
+    public void createFile() throws IOException {
+        File fileObj = new File(this.newFileName);
+        if (fileObj.createNewFile()) {
+            System.out.println("File created: " + fileObj.getName());
+        } else {
+            System.out.println("File with the given name already exists...");
+        }
+    }
+
+    public void writeToFile(String content) throws IOException {
+        FileWriter writer = new FileWriter(this.newFileName);
+        writer.write(content);
+        System.out.println("Successfully wrote to the file.");
+    }
+
+    // Read all .java files related to each individual project folder and save them into an output .txt file
+    public void readFilesFromPath(ArrayList<String[]> files) throws IOException {
+        String content = "";
         for (int i = 0; i < this.filePaths.size(); i++) {
             String[] a = files.get(i);
             for (int j = 0; j < a.length; j++) {
 
                 String filePath;
 
-                if (!(this.filePaths.get(i).endsWith("/"))) {
+                if (!(this.filePaths.get(i).endsWith("/")))  {
                     filePath = this.filePaths.get(i) + "/" +a[j];
-                    System.out.println(filePath);
                 } else {
                     filePath = this.filePaths.get(i)+a[j];
-                    System.out.println(filePath);
                 }
 
+                if (filePath.endsWith(".java")) {
+                    //System.out.println(filePath);
+                    //System.out.println( readJavaFile(filePath) );
+                    content += readJavaFile(filePath);
+                }
             }
         }
+        createFile();
+        writeToFile(content);
+        //System.out.println(content);
     }
 
-    // Return files inside given
-    /*public ArrayList getFilesContained() {
-        ArrayList<String> containedFiles = null;
-        for (int i = 0; i < this.filePaths.size(); i++) {
-
-            containedFiles.add(this.filePaths.get(i));
-
-        }
-        return containedFiles;
-    }*/
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         ArrayList<String> filePaths = new ArrayList<>();
+        filePaths.add("/C:/Projects/School/Ohjelmointi2/viikko2/tehtava1/src/");
         filePaths.add("/C:/Projects/School/Ohjelmointi2/viikko2/tehtava2/src/");
         filePaths.add("/C:/Projects/School/Ohjelmointi2/viikko2/tehtava3/src");
+        filePaths.add("/C:/Projects/School/Ohjelmointi2/viikko2/tehtava4/src/");
+        filePaths.add("/C:/Projects/School/Ohjelmointi2/viikko2/tehtava7/src");
 
-        HandleFiles input1 = new HandleFiles("txt", "asd", filePaths);
+        // Add test to see if file ends with .txt
+        HandleFiles input1 = new HandleFiles("output.txt", "asd", filePaths);
         //ArrayList<String[]> output = input1.listFilesFromEach();
         //System.out.println(output);
 
